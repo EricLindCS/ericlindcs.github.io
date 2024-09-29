55ff217d35c3275d6671ffca042a3532465469da
Eric Lind <107385095+EricLindCS@users.noreply.github.com>
2024-09-29 01:48:52 -0700

    Add Links Page

--- linkspage/links.js
+++ linkspage/links.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     function loadContentFromCSV() {
         $.ajax({
             type: "GET",
-            url: "https://raw.githubusercontent.com/EricLindCS/ericlindcs.github.io/refs/heads/main/linkspage/links.csv",
+            url: "https://raw.githubusercontent.com/EricLindCS/ericlindcs.github.io/refs/heads/links-page/linkspage/links.csv",
             dataType: "text",
             success: function(data) {
                 processData(data);
@@ -32,49 +32,35 @@ $(document).ready(function() {
     function processData(csv) {
         var navContainer = $("#nav-links");
         var contentContainer = $("#main-content");
-
+    
         var lines = csv.split("\n");
         var headers = lines[0].split(",");
-
-        for (var i = 1; i < lines.length-1; i++) {
+    
+        for (var i = 1; i < lines.length - 1; i++) {
             var data = lines[i].split(",");
-          
-                // Populate navigation links
-                var navLink = '<li><a href="#' + data[0] + '">' + data[1] + '</a></li>';
-                navContainer.append(navLink);
-
-                // Populate main content
-                var sectionHtml = '<section id="' + data[0] + '">';
-                sectionHtml += '<h2>' + data[1] + '</h2>';
-                sectionHtml += '<p>' + data[2] + '</p>';
-                sectionHtml += '<ul>';
-                for (var j = 3; j < data.length; j += 2) {
-                    var linkUrl = data[j];
-                    var linkText = data[j + 1];
-                    if (linkUrl && linkText) {
-                        sectionHtml += '<li><a href="' + linkUrl + '">' + linkText + '</a></li>';
-                    }
-                }
-                sectionHtml += '</ul>';
-                sectionHtml += '</section>';
-
-                contentContainer.append(sectionHtml);
-          
-        }
-    }
-
-    function displayFileContents() {
-        $.ajax({
-            type: "GET",
-            url: "links.csv",
-            dataType: "text",
-            success: function(csvData) {
-                console.log("Contents of links.csv:", csvData);
-            },
-            error: function(xhr, status, error) {
-                console.error("Failed to load CSV file:", status, error);
+    
+            // Populate navigation links
+            var navLink = '<li><a href="#' + data[0] + '">' + data[1] + '</a></li>';
+            navContainer.append(navLink);
+    
+            // Populate main content
+            var sectionHtml = '<section id="' + data[0] + '">';
+            sectionHtml += '<h2>' + data[1] + '</h2>';
+            sectionHtml += '<p>' + data[2] + '</p>';
+            sectionHtml += '<ul>';
+            for (var j = 3; j < data.length; j += 3) {
+                var linkTitle = data[j];
+                var linkUrl = data[j + 1];
+                var linkDescription = data[j + 2];
+                //if (linkTitle && linkUrl && linkDescription) {
+                    sectionHtml += '<li><a href="' + linkUrl + '">' + linkTitle + '</a><p>' + linkDescription + '</p></li>';
+                //}
             }
-        });
+            sectionHtml += '</ul>';
+            sectionHtml += '</section>';
+    
+            contentContainer.append(sectionHtml);
+        }
     }
 
     const sidebar = document.getElementById('sidebar');
@@ -84,22 +70,6 @@ $(document).ready(function() {
         sidebar.classList.toggle('collapsed');
     });
 
-    // Existing code
-    function displayFileContents() {
-        $.ajax({
-            type: "GET",
-            url: "links.csv",
-            dataType: "text",
-            success: function(csvData) {
-                console.log("Contents of links.csv:", csvData);
-            },
-            error: function(xhr, status, error) {
-                console.error("Failed to load CSV file:", status, error);
-            }
-        });
-    }
-
     loadContentFromCSV();
-    displayFileContents();
 });
 
